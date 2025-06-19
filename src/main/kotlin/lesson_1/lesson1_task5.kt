65a999371ad11fbe7fe218b40a813d56acf9bd53
Барышников Артём Олегович <abaryshnikov@samokat.ru>
2025-06-19 11:48:36 +0300

    KS-1-5 create task

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,11 @@
 package org.example.lesson_1
 
 fun main() {
+    val secMin: Int = 60
     val secondsFull: Short = 6480
-    val hours: Int = (secondsFull / 60) / 60
-    val minutes: Int = (secondsFull / 60) % 60
-    val seconds: Int = secondsFull % 60
+    val hours: Int = (secondsFull / secMin) / secMin
+    val minutes: Int = (secondsFull / secMin) % secMin
+    val seconds: Int = secondsFull % secMin
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
