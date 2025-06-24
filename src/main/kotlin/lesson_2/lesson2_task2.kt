e3146bbae8054a676cb6458c64b00bd7a958eb86
Барышников Артём Олегович <abaryshnikov@samokat.ru>
2025-06-24 08:25:21 +0300

    KS-2-2 update task

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,19 +1,17 @@
 package org.example.lesson_2
 
 fun main() {
-    // количество сотрудников
     val oldEmployees: Int = 50
     val newEmployees: Int = 30
 
-    // зарплаты сотрудников
     val salaryOldEmployees: Int = 30000
     val salaryNewEmployees: Int = 20000
 
-    // расходы на выплату зарплаты постоянных сотрудников
     val expensesSalaryOldEmployees: Int = oldEmployees * salaryOldEmployees
-    // общие расходы по ЗП после прихода стажеров
     val totalExpensesSalaryEmployees: Int = oldEmployees * salaryOldEmployees + newEmployees * salaryNewEmployees
-    // средняя ЗП одного сотрудника после устройства стажеров
     val averageSalary: Int = (oldEmployees * salaryOldEmployees + newEmployees * salaryNewEmployees) / (oldEmployees + newEmployees)
 
+    println("Расходы на выплату зарплаты постоянных сотрудников: $expensesSalaryOldEmployees")
+    println("Общие расходы по ЗП после прихода стажеров: $totalExpensesSalaryEmployees")
+    println("Средняя ЗП одного сотрудника после устройства стажеров $averageSalary")
 }
