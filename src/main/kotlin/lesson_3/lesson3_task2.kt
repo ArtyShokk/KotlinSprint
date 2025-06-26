868e2366376ec6fc7d1efae23bba0efb9686ea83
Барышников Артём Олегович <abaryshnikov@samokat.ru>
2025-06-26 14:56:44 +0300

    KS-3-2 create task

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val maidenName: String = "Андреева"
+    val surname: String = "Сидорова"
+    val name: String = "Татьяна"
+    val patronymic: String = "Сергеевна"
+    val ageBeforeMarriage: Int = 20
+    val age: Int = 22
+
+    println("$maidenName $name $patronymic, $ageBeforeMarriage")
+    println("$surname $name $patronymic, $age")
+
+
+}
