dc6038d4ddf90cb85e74550fba480dceb4f3f8d4
Барышников Артём Олегович <abaryshnikov@samokat.ru>
2025-06-26 10:14:51 +0300

    KS-2-5 update task

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -6,9 +6,10 @@ fun main() {
     val initialDepositAmount: Int = 70_000
     val depositInterestRate: Double = 16.7
     val depositTerm: Int = 20
+    val percent: Int = 100
 
-    val finalAmount: Double = initialDepositAmount * (1 + depositInterestRate / 100).pow(depositTerm.toDouble())
+    val finalAmount: Double = initialDepositAmount * (1 + depositInterestRate / percent).pow(depositTerm.toDouble())
 
-    println("Размер вклада через 20 лет составит: ${String.format("%.3f", finalAmount)}")
+    println("Размер вклада через 20 лет составит: \"%.3f\"".format(finalAmount))
 
 }
