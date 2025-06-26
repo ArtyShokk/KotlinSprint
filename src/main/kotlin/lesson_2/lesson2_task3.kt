0027a47d8f7e2f87c444c5e98267af32ab0c30b0
Барышников Артём Олегович <abaryshnikov@samokat.ru>
2025-06-26 14:19:10 +0300

    KS-2-3 update task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,17 +1,18 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+const val HOURS_IN_DAY = 24
+
 fun main() {
-    val minutesInHour = 60
-    val hoursInDay = 24
     val hours = 9
     val minutes = 39
-    val departureTimeMinutes = hours * minutesInHour + minutes
+    val departureTimeMinutes = hours * MINUTES_IN_HOUR + minutes
     val travelTimeMinutes = 457
 
     val arrivalTimeMinutes = departureTimeMinutes + travelTimeMinutes
 
-    val arrivalHour = (arrivalTimeMinutes / minutesInHour) % hoursInDay
-    val arrivalMinute = arrivalTimeMinutes % minutesInHour
+    val arrivalHour = (arrivalTimeMinutes / MINUTES_IN_HOUR) % HOURS_IN_DAY
+    val arrivalMinute = arrivalTimeMinutes % MINUTES_IN_HOUR
 
     println("Поезд прибудет в %02d:%02d".format(arrivalHour, arrivalMinute))
 }
