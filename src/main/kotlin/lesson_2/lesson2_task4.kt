0745e8a4cab1c158d00c4bc0de5579a4b5939969
Барышников Артём Олегович <abaryshnikov@samokat.ru>
2025-06-26 10:10:19 +0300

    KS-2-4 update task

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,9 +4,10 @@ fun main() {
     val crystals: Int = 7
     val ironOre: Int = 11
     val buffPercentage: Int = 20
+    val percent: Int = 100
 
-    val bonusCrystals: Int = (crystals * buffPercentage / 100).toInt()
+    val bonusCrystals: Int = (crystals * buffPercentage / percent).toInt()
     println("Бонусные кристаллы: $bonusCrystals")
-    val bonusIronOre: Int = (ironOre * buffPercentage / 100).toInt()
+    val bonusIronOre: Int = (ironOre * buffPercentage / percent).toInt()
     println("Бонусная железная руда: $bonusIronOre")
 }
